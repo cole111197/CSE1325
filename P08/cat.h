51c0520e0f76795a7b06a3113f7e22b639151bf2
cole111197 <cole.montgomery@mavs.uta.edu>
2019-11-07 17:02:37 -0600

    Cat class created

---
+++ P08/cat.h
@@ -0,0 +1,37 @@
+#ifndef __CAT_H
+#define __CAT_H
+
+#include "animal.h"
+
+enum class Cat_breed{
+    PERSIAN,
+    SIAMESE,
+    MAINE_COON,
+    RAGDOLL,
+    MUNCHKIN,
+    RUSSIAN_BLUE,
+    SPHYNX,
+    BALINESE
+};
+constexpr Cat_breed cat_breeds[] = {
+    Cat_breed::PERSIAN,
+    Cat_breed::SIAMESE,
+    Cat_breed::MAINE_COON,
+    Cat_breed::RAGDOLL,
+    Cat_breed::MUNCHKIN,
+    Cat_breed::RUSSIAN_BLUE,
+    Cat_breed::SPHYNX,
+    Cat_breed::BALINESE
+};
+std::string to_string(const Cat_breed& breed);
+std::ostream& operator<<(std::ostream& ost, const Cat_breed& breed);
+
+class Cat : public Animal {
+    public:
+        Cat(Cat_breed breed, std::string name, Gender gender, int age);
+        virtual ~Cat();
+    private:
+        Cat_breed _breed;
+};
+
+#endif
