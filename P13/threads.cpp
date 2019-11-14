3ed9984f59cd06c00f2664377004c4a095c08847
cole111197 <cole.montgomery@mavs.uta.edu>
2019-11-14 01:03:21 -0600

    threads working and results.txt updated

--- P13/threads.cpp
+++ P13/threads.cpp
@@ -38,15 +38,12 @@ class Prime_numbers {
       }
     }
 
-    void find_primes(int max_int){
-      std::thread threads[NUM_THREADS];
-      for(int i = 0; i < NUM_THREADS; i++){
-        threads[i] = std::thread(&Prime_numbers::find_primes_thread, this, ((i*(max_int/NUM_THREADS))+1), ((i+1)*(max_int/NUM_THREADS)));
-        
-        //std::cout << std::to_string((i*(max_int/4))+1) << " -> " << std::to_string((i+1)*(max_int/4)) << std::endl;
-        //find_primes((i*(max_int/NUM_THREADS))+1, (i+1)*(max_int/NUM_THREADS));
+    void find_primes(int num_threads, int max_int){
+      std::thread threads[num_threads];
+      for(int i = 0; i < num_threads; i++){
+        threads[i] = std::thread(&Prime_numbers::find_primes_thread, this, ((i*(max_int/num_threads))+1), ((i+1)*(max_int/num_threads)));
       }
-      for(int i = 0; i < NUM_THREADS; i++){
+      for(int i = 0; i < num_threads; i++){
         threads[i].join();
       }
     }
@@ -77,8 +74,7 @@ int main(int argc, char* argv[]) {
     if(argc > 2) max_int = atoi(argv[2]);
 
     // Search and identify all primes between 2 and max_int
-    //prime_numbers.find_primes(2, max_int);
-    prime_numbers.find_primes(max_int);
+    prime_numbers.find_primes(threads, max_int);
 
     // Print all primes that were found  
     for (int p : prime_numbers) std::cout << p << '\n';
